c3e5952fa0ffc1ab8f5026ea3dfff7876a0a2f7d
pouria1370 <kalantari_pouria@yahoo.com>
2022-09-10 12:11:21 +0430

    it is now a base outline for my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Aboutme from "./Pages/Aboutme";
+import Charackter from "./Pages/Charackter";
+import LandingPage from "./Pages/LandingPage";
+import NoOne from "./Pages/NoOne";
+import Resume from "./Pages/Resume";
+import Toturials from "./Pages/Toturials";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
-}
-
+};
 export default App;
